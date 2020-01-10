442a22ba054a04c82e0df35686b6cb904c36750f
Arthur Gassner <arthur.gassner@gmail.com>
2020-01-10 15:40:03 +0000

    Fix tests

--- test/test_tf_backend_service_routing.py
+++ test/test_tf_backend_service_routing.py
@@ -45,21 +45,33 @@ Plan: 2 to add, 0 to change, 0 to destroy.
         # Then
         assert """
 + module.backend_service_routing.aws_alb_listener_rule.rule
-      id:                                 <computed>
-      action.#:                           "1"
-      action.0.order:                     <computed>
-      action.0.target_group_arn:          "${aws_alb_target_group.target_group.arn}"
-      action.0.type:                      "forward"
-      arn:                                <computed>
-      condition.#:                        "2"
-      condition.1746742835.field:         "path-pattern"
-      condition.1746742835.values.#:      "1"
-      condition.1746742835.values.0:      "*"
-      condition.3778616515.field:         "host-header"
-      condition.3778616515.values.#:      "1"
-      condition.3778616515.values.0:      "dev-cognito.domain.com"
-      listener_arn:                       "alb:listener"
-      priority:                           "10"
+      id:                                         <computed>
+      action.#:                                   "1"
+      action.0.order:                             <computed>
+      action.0.target_group_arn:                  "${aws_alb_target_group.target_group.arn}"
+      action.0.type:                              "forward"
+      arn:                                        <computed>
+      condition.#:                                "2"
+      condition.1322904213.field:                 "path-pattern"
+      condition.1322904213.host_header.#:         <computed>
+      condition.1322904213.http_header.#:         "0"
+      condition.1322904213.http_request_method.#: "0"
+      condition.1322904213.path_pattern.#:        <computed>
+      condition.1322904213.query_string.#:        "0"
+      condition.1322904213.source_ip.#:           "0"
+      condition.1322904213.values.#:              "1"
+      condition.1322904213.values.0:              "*"
+      condition.3843014500.field:                 "host-header"
+      condition.3843014500.host_header.#:         <computed>
+      condition.3843014500.http_header.#:         "0"
+      condition.3843014500.http_request_method.#: "0"
+      condition.3843014500.path_pattern.#:        <computed>
+      condition.3843014500.query_string.#:        "0"
+      condition.3843014500.source_ip.#:           "0"
+      condition.3843014500.values.#:              "1"
+      condition.3843014500.values.0:              "dev-cognito.domain.com"
+      listener_arn:                               "alb:listener"
+      priority:                                   "10"
         """.strip() in output
 
     def test_create_alb_listener_rule_live(self):
@@ -80,21 +92,33 @@ Plan: 2 to add, 0 to change, 0 to destroy.
         # Then
         assert """
 + module.backend_service_routing.aws_alb_listener_rule.rule
-      id:                                 <computed>
-      action.#:                           "1"
-      action.0.order:                     <computed>
-      action.0.target_group_arn:          "${aws_alb_target_group.target_group.arn}"
-      action.0.type:                      "forward"
-      arn:                                <computed>
-      condition.#:                        "2"
-      condition.1746742835.field:         "path-pattern"
-      condition.1746742835.values.#:      "1"
-      condition.1746742835.values.0:      "*"
-      condition.1997051849.field:         "host-header"
-      condition.1997051849.values.#:      "1"
-      condition.1997051849.values.0:      "cognito.domain.com"
-      listener_arn:                       "alb:listener"
-      priority:                           "10"
+      id:                                         <computed>
+      action.#:                                   "1"
+      action.0.order:                             <computed>
+      action.0.target_group_arn:                  "${aws_alb_target_group.target_group.arn}"
+      action.0.type:                              "forward"
+      arn:                                        <computed>
+      condition.#:                                "2"
+      condition.1322904213.field:                 "path-pattern"
+      condition.1322904213.host_header.#:         <computed>
+      condition.1322904213.http_header.#:         "0"
+      condition.1322904213.http_request_method.#: "0"
+      condition.1322904213.path_pattern.#:        <computed>
+      condition.1322904213.query_string.#:        "0"
+      condition.1322904213.source_ip.#:           "0"
+      condition.1322904213.values.#:              "1"
+      condition.1322904213.values.0:              "*"
+      condition.4207679377.field:                 "host-header"
+      condition.4207679377.host_header.#:         <computed>
+      condition.4207679377.http_header.#:         "0"
+      condition.4207679377.http_request_method.#: "0"
+      condition.4207679377.path_pattern.#:        <computed>
+      condition.4207679377.query_string.#:        "0"
+      condition.4207679377.source_ip.#:           "0"
+      condition.4207679377.values.#:              "1"
+      condition.4207679377.values.0:              "cognito.domain.com"
+      listener_arn:                               "alb:listener"
+      priority:                                   "10"
         """.strip() in output
 
     def test_create_aws_alb_target_group(self):
@@ -115,32 +139,32 @@ Plan: 2 to add, 0 to change, 0 to destroy.
         # Then
         assert """
   + module.backend_service_routing.aws_alb_target_group.target_group
-      id:                                 <computed>
-      arn:                                <computed>
-      arn_suffix:                         <computed>
-      deregistration_delay:               "10"
-      health_check.#:                     "1"
-      health_check.0.enabled:             "true"
-      health_check.0.healthy_threshold:   "2"
-      health_check.0.interval:            "5"
-      health_check.0.matcher:             "200-299"
-      health_check.0.path:                "/internal/healthcheck"
-      health_check.0.port:                "traffic-port"
-      health_check.0.protocol:            "HTTP"
-      health_check.0.timeout:             "4"
-      health_check.0.unhealthy_threshold: "2"
-      lambda_multi_value_headers_enabled: "false"
-      name:                               "dev-cognito-service"
-      port:                               "31337"
-      protocol:                           "HTTP"
-      proxy_protocol_v2:                  "false"
-      slow_start:                         "0"
-      stickiness.#:                       <computed>
-      tags.%:                             "3"
-      tags.component:                     "cognito-service"
-      tags.env:                           "dev"
-      tags.service:                       "dev-cognito-service"
-      target_type:                        "instance"
-      vpc_id:                             "vpc-12345678"
+      id:                                         <computed>
+      arn:                                        <computed>
+      arn_suffix:                                 <computed>
+      deregistration_delay:                       "10"
+      health_check.#:                             "1"
+      health_check.0.enabled:                     "true"
+      health_check.0.healthy_threshold:           "2"
+      health_check.0.interval:                    "5"
+      health_check.0.matcher:                     "200-299"
+      health_check.0.path:                        "/internal/healthcheck"
+      health_check.0.port:                        "traffic-port"
+      health_check.0.protocol:                    "HTTP"
+      health_check.0.timeout:                     "4"
+      health_check.0.unhealthy_threshold:         "2"
+      lambda_multi_value_headers_enabled:         "false"
+      name:                                       "dev-cognito-service"
+      port:                                       "31337"
+      protocol:                                   "HTTP"
+      proxy_protocol_v2:                          "false"
+      slow_start:                                 "0"
+      stickiness.#:                               <computed>
+      tags.%:                                     "3"
+      tags.component:                             "cognito-service"
+      tags.env:                                   "dev"
+      tags.service:                               "dev-cognito-service"
+      target_type:                                "instance"
+      vpc_id:                                     "vpc-12345678"
         """.strip() in output
 
